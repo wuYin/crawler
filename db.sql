ff3c64cedca8be20884b9f4905094497e3f4a25d
wuYin <wuYinPost@gmail.com>
2018-01-16 18:52:11 +0800

    修改表结构

--- db.sql
+++ db.sql
@@ -1,7 +1,8 @@
 CREATE TABLE danmu (
   id         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  room_id    INT          NOT NULL             DEFAULT 0,
   uid        INT          NOT NULL             DEFAULT 0,
-  uname      VARCHAR(255) NOT NULL             DEFAULT '',
+  uname      VARCHAR(255) NOT NULL             DEFAULT '' ,
   level      TINYINT      NOT NULL             DEFAULT 0
   COMMENT '用户等级 ul',
   content    VARCHAR(255) NOT NULL             DEFAULT ''
@@ -9,10 +10,12 @@ CREATE TABLE danmu (
   created_at TIMESTAMP    NOT NULL             DEFAULT NOW(),
   updated_at TIMESTAMP    NOT NULL             DEFAULT NOW() ON UPDATE NOW()
 )
-  CHARSET = utf8;
+  CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 CREATE TABLE gifts (
   id         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  room_id    INT          NOT NULL             DEFAULT 0,
   uid        INT          NOT NULL             DEFAULT 0,
   gift_id    INT          NOT NULL             DEFAULT 0,
   gift_name  VARCHAR(255) NOT NULL             DEFAULT '',
@@ -21,4 +24,5 @@ CREATE TABLE gifts (
   created_at TIMESTAMP    NOT NULL             DEFAULT NOW(),
   updated_at TIMESTAMP    NOT NULL             DEFAULT NOW() ON UPDATE NOW()
 )
-  CHARSET = utf8;
+  CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
