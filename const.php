d76b0ff269912632a737b3b6fbffe1c1431106e4
wuYin <wuYinPost@gmail.com>
2018-01-16 15:23:21 +0800

    提取常量

---
+++ const.php
@@ -0,0 +1,13 @@
+<?php
+
+const ROOM_INIT_API   = 'https://api.live.bilibili.com/room/v1/Room/room_init?id=';
+const ROOM_SERVER_API = 'https://api.live.bilibili.com/api/player?id=cid:';
+
+const ACTION_ENTRY     = 7;
+const ACTION_HEARTBEAT = 2;
+const UID              = 18466419;
+
+const DB_NAME   = 'bilibili';
+const DB_HOST   = '127.0.0.1';
+const DB_USER   = 'root';
+const DB_PASSWD = '123456';
