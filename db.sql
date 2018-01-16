b5a3fd8f94ab6893f6f295078bc79752c17bfd4a
wuYin <wuYinPost@gmail.com>
2018-01-16 15:21:20 +0800

    修改表结构

--- db.sql
+++ db.sql
@@ -1,16 +1,24 @@
 CREATE TABLE danmu (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  uid INT NOT NULL DEFAULT 0,
-  uname VARCHAR(255) NOT NULL DEFAULT '',
-  level TINYINT NOT NULL DEFAULT 0 COMMENT '用户等级 ul',
-  content VARCHAR(255) NOT NULL DEFAULT '' COMMENT '弹幕内容'
-);
+  id         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  uid        INT          NOT NULL             DEFAULT 0,
+  uname      VARCHAR(255) NOT NULL             DEFAULT '',
+  level      TINYINT      NOT NULL             DEFAULT 0
+  COMMENT '用户等级 ul',
+  content    VARCHAR(255) NOT NULL             DEFAULT ''
+  COMMENT '弹幕内容',
+  created_at TIMESTAMP    NOT NULL             DEFAULT NOW(),
+  updated_at TIMESTAMP    NOT NULL             DEFAULT NOW() ON UPDATE NOW()
+)
+  CHARSET = utf8;
 
 CREATE TABLE gifts (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  uid INT NOT NULL DEFAULT 0,
-  gift_id INT NOT NULL DEFAULT 0,
-  gift_name VARCHAR(255) NOT NULL DEFAULT '',
-  price INT NOT NULL DEFAULT 0.0,
-  num INT NOT NULL DEFAULT 0
-);
+  id         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  uid        INT          NOT NULL             DEFAULT 0,
+  gift_id    INT          NOT NULL             DEFAULT 0,
+  gift_name  VARCHAR(255) NOT NULL             DEFAULT '',
+  price      INT          NOT NULL             DEFAULT 0.0,
+  num        INT          NOT NULL             DEFAULT 0,
+  created_at TIMESTAMP    NOT NULL             DEFAULT NOW(),
+  updated_at TIMESTAMP    NOT NULL             DEFAULT NOW() ON UPDATE NOW()
+)
+  CHARSET = utf8;
