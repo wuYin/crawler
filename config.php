7fe308bd2ee3af7b0ec269c122aa983635ee7839
wuYin <wuYinPost@gmail.com>
2018-03-30 12:25:29 +0800

    修改配置文件

--- config.php
+++ config.php
@@ -1,4 +1,13 @@
 <?php
 
-const FFMPEG_EXEC_PATH = '/Users/wuyin/Desktop/ffmpeg/bin/ffmpeg';
+/**
+ * ffmpeg 的可执行文件路径
+ * macOS 可使用 brew install ffmpeg 安装
+ * linux 可到 ffmpeg.org/download.html#build-linux 下载，编译安装后指定 bin/ffmpeg 的绝对路径即可
+ */
+const FFMPEG_EXEC_PATH = 'ffmpeg';
+
+/**
+ * 录像的保存路径
+ */
 const VIDEO_SAVE_DIR   = '/Users/wuyin/Desktop/';
