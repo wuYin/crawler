f90b326eb62eefacd55360e8b3881f3ac7ab7363
wuYin <wuYinPost@gmail.com>
2018-03-30 12:25:20 +0800

    添加爬虫的函数注释

--- crawler.php
+++ crawler.php
@@ -3,20 +3,29 @@ require_once 'functions.php';
 
 $timestamp = 0;
 
+// 参数检查
 $shortID = $argv[1];
 if (!is_numeric($shortID)) {
 	exit('Usage like: php crawler.php 5441');
 }
 
+// 连接到数据库
 $conn   = connectDB();
+
+// 转换热门主播的短房间号为真实房间号
 $roomID = getRealRoomID($shortID);
 
-// recordVideo($roomID);
+// 保存直播录像
+recordVideo($roomID);
 
+// 获取弹幕服务器的地址
 $server  = getServer($roomID);
+
+// 连接到弹幕服务器
 $socket  = connectServer($server['ip'], $server['port'], $roomID);
-$message = decodeMessage($socket);
+
+// 处理弹幕和礼物消息
+decodeMessage($socket);
+
 // analysisDanmu($conn, $roomID);
 // analysisGifts($conn, $roomID);
-
-decodeMessage($socket);
