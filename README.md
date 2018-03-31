1ba36c865f83068bf5a249cba165958d7c5fea58
wuYin <wuYinPost@gmail.com>
2018-03-31 21:01:14 +0800

    修改使用 sequal pro 导出的表结构

--- README.md
+++ README.md
@@ -1,24 +1,81 @@
-B 站直播间弹幕、礼物爬虫，及数据统计、弹幕分析。
+## 概述
 
-\# 弹幕、礼物爬虫：
-
- ![](http://p2j5s8fmr.bkt.clouddn.com/run.png)
+使用 PHP 实现的 B 站直播间弹幕和礼物爬虫、弹幕分析与精彩时刻自动剪辑脚本。
 
 
 
-\# 弹幕分析：
+## 前言
 
- ![](http://p2j5s8fmr.bkt.clouddn.com/words.png)
+### 起因
+
+想当初，我也想做一名 UP，自己做吃鸡的精彩集锦，遇到的第一个困难便是高能的精彩素材。
+
+低效做法：守着主播的直播间，使用录屏软件录播，记下高能时刻，手动剪辑。
+
+高效做法：
+
+- Crontab 定时任务检测直播间开播状态
+- 截获直播间推流地址后，使用 FFmpeg 自动录播和剪辑
+- 大多数用户只在主播有精彩操作时发弹幕送礼物，编写爬虫抓取直播间的弹幕、礼物数据
+- 为弹幕做分词处理，“学不来”、“基本操作”、666、233…等词集中出现时，认为有精彩素材，同时还能做粉丝的弹幕行为分析等等。
+
+### 依据
+
+我和室友都经常看直播，但很少发弹幕和送礼物，只有在主播玩出很溜的操作或讲很好玩的事情时，才会发弹幕互动、送礼物支持。
+
+基于这个用户习惯，不难推断出在直播间的弹幕高峰或礼物高峰期，主播应该做了些好玩的事情，比如惊险吃鸡或全队被灭之类的…这些时间段都可以作为精彩时刻的素材。
 
 
 
-\# 弹幕数据统计：
+### 使用
 
- ![](http://p2j5s8fmr.bkt.clouddn.com/danmu.png)
+在 config.php 和 const.php 中配置数据库后执行 SQL 文件，指定下载好的  [FFmpeg](https://www.ffmpeg.org/download.html) 可执行文件位置。
 
-\# 礼物价值数据统计：
-
-  ![](http://p2j5s8fmr.bkt.clouddn.com/gifts.png)
+代码量 600 行左右，注释写得比较清楚，看源码就 ok
 
 
 
+### 目录结构
+
+```shell
+> bilibili-live-crawler $ tree -L 2
+.
+├── README.md
+├── config.php		# 配置文件：配置 FFmpeg 可执行文件的位置，录像的保存路径
+├── const.php		# 常量文件：API 地址，定义数据库用户名和密码、弹幕激增的判定参数等
+├── crawler.php		# 连接并抓取弹幕服务器的数据
+├── cut_words
+│   └── seg.php		# 分词脚本：将弹幕做分词处理，可用于生成本次直播的词图
+├── db.sql			# 数据存储
+├── edit.php		# 剪辑脚本
+├── functions.php	# 公用函数
+└── visual_data.php	# 直播数据可视化文件脚本
+```
+
+
+
+## 实现效果
+
+### 弹幕、礼物和礼物数据爬虫：
+
+![](http://p2j5s8fmr.bkt.clouddn.com/crawler.png)
+
+### 弹幕分析：
+
+![](http://p2j5s8fmr.bkt.clouddn.com/cuts.png)
+
+
+
+### 弹幕数据统计：
+
+ ![](http://p2j5s8fmr.bkt.clouddn.com/visu.png)
+
+
+
+### 根据弹幕和礼物高峰自动剪辑
+
+![](http://p2j5s8fmr.bkt.clouddn.com/edit_shell.png)
+
+
+
+### 精彩集锦的素材![](http://p2j5s8fmr.bkt.clouddn.com/saved.png)
--- db.sql
+++ db.sql
@@ -1,28 +1,74 @@
-CREATE TABLE danmu (
-  id         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  room_id    INT          NOT NULL             DEFAULT 0,
-  uid        INT          NOT NULL             DEFAULT 0,
-  uname      VARCHAR(255) NOT NULL             DEFAULT '' ,
-  level      TINYINT      NOT NULL             DEFAULT 0
-  COMMENT '用户等级 ul',
-  content    VARCHAR(255) NOT NULL             DEFAULT ''
-  COMMENT '弹幕内容',
-  created_at TIMESTAMP    NOT NULL             DEFAULT NOW(),
-  updated_at TIMESTAMP    NOT NULL             DEFAULT NOW() ON UPDATE NOW()
-)
-  CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.21)
+# Database: bilibili
+# Generation Time: 2018-03-31 12:58:52 +0000
+# ************************************************************
 
-CREATE TABLE gifts (
-  id         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  room_id    INT          NOT NULL             DEFAULT 0,
-  uid        INT          NOT NULL             DEFAULT 0,
-  gift_id    INT          NOT NULL             DEFAULT 0,
-  gift_name  VARCHAR(255) NOT NULL             DEFAULT '',
-  price      INT          NOT NULL             DEFAULT 0.0,
-  num        INT          NOT NULL             DEFAULT 0,
-  created_at TIMESTAMP    NOT NULL             DEFAULT NOW(),
-  updated_at TIMESTAMP    NOT NULL             DEFAULT NOW() ON UPDATE NOW()
-)
-  CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table danmu
+# ------------------------------------------------------------
+
+DROP DATABASE IF EXISTS `bilibili`;
+
+CREATE DATABASE bilibili;
+
+USE bilibili;
+
+DROP TABLE IF EXISTS `danmu`;
+
+CREATE TABLE `danmu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `room_id` int(11) NOT NULL DEFAULT '0',
+  `uid` int(11) NOT NULL DEFAULT '0',
+  `uname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `level` tinyint(4) NOT NULL DEFAULT '0' COMMENT '用户等级 ul',
+  `content` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '弹幕内容',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+# Dump of table gifts
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `gifts`;
+
+CREATE TABLE `gifts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `room_id` int(11) NOT NULL DEFAULT '0',
+  `uid` int(11) NOT NULL DEFAULT '0',
+  `gift_id` int(11) NOT NULL DEFAULT '0',
+  `gift_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `price` int(11) NOT NULL DEFAULT '0',
+  `num` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
